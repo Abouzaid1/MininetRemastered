4f5053e270f3b1bec83fd0aa301de0a698fd2845
Your Name <am5715073@gmail.com>
2024-03-20 01:52:01 +0200

    Abouzaid

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'http://localhost:3000';
+// const URL = 'http://localhost:3000';
+const URL = 'https://mininetremasteredserverside-4.onrender.com/';
 
 export const socket = io(URL, { transports: ['websocket', 'polling', 'flashsocket'] });
