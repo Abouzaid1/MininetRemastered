794eff4e7fe9e62ce33b7f1f4854a45f1dc1c81e
Your Name <am5715073@gmail.com>
2024-03-23 03:46:45 +0200

    ahmed abouzaid

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -2,4 +2,4 @@ import { io } from 'socket.io-client';
 
 const URL = import.meta.env.VITE_APP_URL
 
-export const socket = io("https://light-turquoise-cub.glitch.me/", { transports: ['websocket'] });
+export const socket = io("https://light-turquoise-cub.glitch.me/");
