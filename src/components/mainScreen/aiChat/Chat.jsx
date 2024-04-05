feb3cf589a485afa07eb58e109598e89b5bf52a1
Your Name <am5715073@gmail.com>
2024-04-05 20:44:16 +0200

    ahmed abouzaid

--- src/components/mainScreen/aiChat/Chat.jsx
+++ src/components/mainScreen/aiChat/Chat.jsx
@@ -9,67 +9,34 @@ export default function Chat() {
     const [message, setMessage] = useState('')
     const [output, setOutput] = useState()
     const [loading, setLoading] = useState(false)
-    // const safetySettings = [
-    //     { category: 'HARM_CATEGORY_SEXUALLY_EXPLICIT', probability: 'LOW' },
-    //     { category: 'HARM_CATEGORY_HATE_SPEECH', probability: 'NEGLIGIBLE' },
-    //     { category: 'HARM_CATEGORY_HARASSMENT', probability: 'HIGH' },
-    //     {
-    //         category: 'HARM_CATEGORY_DANGEROUS_CONTENT',
-    //         probability: 'NEGLIGIBLE'
-    //     }
-    // ];
-    // const genAI = new GoogleGenerativeAI("AIzaSyCSsTjxFSuCWHixWhpLd2V1lydGSXFlp7I");
-    // const clickHandler = async () => {
-    //     if (message !== "") {
-
-    //         setLoading(true);
-    //         const model = genAI.getGenerativeModel({ model: "gemini-pro" ,safetySettings});
-    //         setOutput("")
-    //         const prompt = message
-
-    //         const result = await model.generateContent(prompt);
-    //         const response = await result.response;
-    //         const text = response.text();
-    //         if (text) {
-    //             setLoading(false);
-    //         }
-    //         setOutput(text.replaceAll("*", ""))
-
-    //         setMessage("")
-    //     }
-    //     // console.log(import.meta.env.API_KEY);
-    // }
-    // __________________________________________________________________________________________________
-    const apiKey = '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';
-    const endpoint = 'https://api.openai.com/v1/models';
-
+    const safetySettings = [
+        {
+            category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+            threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+        },
+    ];
+    const genAI = new GoogleGenerativeAI("AIzaSyCSsTjxFSuCWHixWhpLd2V1lydGSXFlp7I");
     const clickHandler = async () => {
-        setLoading(true)
-        const prompt = message;
-        const requestData = {
-            prompt: prompt,
-            max_tokens: 50, // Adjust as needed
-            temperature: 0.7, // Adjust as needed
-            n: 1 // Number of completions to generate
-        };
+        if (message !== "") {
 
-        // Define the request headers
-        const headers = {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${apiKey}`
-        };
+            setLoading(true);
+            const model = genAI.getGenerativeModel({ model: "gemini-pro", safetySettings });
+            setOutput("")
+            const prompt = message
 
-        // Send the request
-        axios.post(endpoint, requestData, { headers: headers })
-            .then(response => {
-                console.log(response.data.choices[0].text);
-                setOutput(response.data.choices[0].text)
-                setLoading(false)
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
+            const result = await model.generateContent(prompt);
+            const response = await result.response;
+            const text = response.text();
+            if (text) {
+                setLoading(false);
+            }
+            setOutput(text.replaceAll("*", ""))
+
+            setMessage("")
+        }
+        // console.log(import.meta.env.API_KEY);
     }
+    // __________________________________________________________________________________________________
     return (
         <div className='min-w-[300px] transition-all p-5 max-w-[500px] min-h-[70px] max-h-[600px] z-30 bg-secondary absolute right-[20px] rounded-[10px] top-[100px] overflow-y-scroll'>
             {/* <p className='text-white font-semibold text-[30px]'>Gemini</p> */}
