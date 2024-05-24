818a89f57bb7f63b623c177547ebac69d3770ed6
Your Name <am5715073@gmail.com>
2024-05-25 01:11:31 +0300

    abouzaid

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   server: {
     host: '0.0.0.0',
     port: 5000,  // you can choose a different port if needed
